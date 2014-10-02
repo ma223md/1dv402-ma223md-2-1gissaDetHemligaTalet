02b8bac979ab92ffb8c88f2ef045958d8a254f81
ma223md <ma223md@student.lnu.se>
2014-10-02 09:24:37 +0200

    Finished lab 2.1

--- SecretNumber.cs
+++ SecretNumber.cs
@@ -6,20 +6,16 @@ namespace _1DV402.S2.L1A
     {
             public const int MaxNumberOfGuesses = 7;
 
-            // _count räknar antalet gissningar som gjorts, om makeguess kallats 7ggr
             private int _count;
-
-            // _number innehåller det hemliga talet
             private int _number;
 
-            // konstruktor SecretNumber()
             public SecretNumber()
             {
                 //åkalla initialize för att initiera fälten
                 Initialize();
             }
 
-            // Initialize = publik metod som initierar fältens värden
+            // Metod som initierar fältens värden
             public void Initialize()
             {
                 _count = 0;
@@ -29,33 +25,46 @@ namespace _1DV402.S2.L1A
 
             public bool MakeGuess(int number)
             {
-                // Meddelar om värdet är rätt, för högt eller för lågt
-                if (number == _number)
+                bool guess = false;
+
+                // Om gissningen inte är i intervallet 1-100 kastas undantag
+                if (number > 100 || number < 1)
                 {
-                    Console.WriteLine("Rätt gissat! Du klarade det på {0} försök", _count);
+                    throw new ArgumentOutOfRangeException();
                 }
 
-                if (number < _number)
-                {
-                    Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar", number, _count);
-                }
-
-                if (number > _number)
-                {
-                    Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar", number, _count);
-                }
                 // Höjer _count tills max antal gissningar nåtts
                 if (_count < MaxNumberOfGuesses)
                 {
-                    _count++;
+                    _count++;                    
+                } 
+                else
+                {
+                    throw new ApplicationException();
                 }
 
-                // Meddelar det hemliga talet om användaren har slut på gissningar
+                // Meddelar om värdet är för högt eller för lågt
+                if (number < _number)
+                {
+                    Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                }
+                else if (number > _number)
+                {
+                    Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                }
+                //Meddelar om värder är rätt
+                else
+                {
+                    Console.WriteLine("Rätt gissat! Du klarade det på {0} försök", _count);
+                    guess = true;
+                }
+
+                // Meddelar det hemliga talet om användaren har slut på gissningar  
                 if (_count == MaxNumberOfGuesses)
                 {
                     Console.WriteLine("Det hemliga talet är {0}", _number);
-                }
-                return true;
+                }              
+                return guess;
             }
     }
 }
